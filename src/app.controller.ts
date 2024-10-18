898eaed015bd6d7134dcc9611cb78517b41e2a24
Petrik Diak <diak@petrik.hu>
2024-10-18 15:49:42 +0200

    elso

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Put, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ReplaceProductDto } from './replaceProduct.dto';
 
 @Controller()
 export class AppController {
@@ -12,4 +13,54 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+
+  #productList = [{
+    name: "Washing machine",
+    price: 299000
+    },
+    {
+      name: "Basketball",
+      price: 20000
+    },
+    {
+      name: "Football",
+      price: 30000
+    }
+  ]
+
+
+  @Get('products')
+  listProducts(){
+    return this.#productList
+  }
+
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string){
+    return JSON.stringify(this.#productList[id])
+  }
+
+  @Delete('products/:id')
+  deleteProduct(@Param('id') id: string){
+
+    if(!this.#productList[id]){
+      throw new NotFoundException('No product with this ID')
+    }
+    this.#productList.splice(Number(id), 1)
+
+  }
+
+  @Put('products/:id')
+  replaceProduct(@Param('id') id: string, @Body() data: ReplaceProductDto){
+    if(!this.#productList[id]){
+      throw new NotFoundException('No product with this ID')
+    } 
+    this.#productList[id] = data.name
+
+    
+  }
+
 }
+
+
+
